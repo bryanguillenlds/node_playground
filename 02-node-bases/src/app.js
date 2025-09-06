c53dbff0a489f86e61ce59905a9514646b0539f0
Bryan Guillén <bryanguillenlds@gmail.com>
2025-09-06 14:22:07 -0400

    Practicing Adapter pattern and Factory pattern with DI.

--- 02-node-bases/src/app.js
+++ 02-node-bases/src/app.js
@@ -2,7 +2,8 @@
 import { emailTemplate } from "./js-bases/01-template.js";
 import { SHELL, NVM_DIR } from "./js-bases/02-destructuring.js";
 import { getUserByName } from "./js-bases/03-callbacks.js";
-import { buildPerson, personObject } from "./js-bases/05-factory-functions.js";
+import { getId, getAge } from "./plugins/index.plugin.js";
+import { buildMakePerson } from "./js-bases/05-factory-functions.js";
 // console.log(emailTemplate);
 // console.log(SHELL, NVM_DIR);
 getUserByName("John",  (error, user) => {
@@ -12,4 +13,35 @@ getUserByName("John",  (error, user) => {
   console.log(user);
 });
 
-console.log(buildPerson(personObject));
+/**
+ * DEPENDENCY INJECTION: Injecting the actual implementations into the factory
+ *
+ * buildMakePerson is a factory function that expects dependencies as parameters.
+ * Here we're injecting the real implementations (getId, getAge) that will be
+ * "baked into" the returned makePerson function.
+ */
+const makePerson = buildMakePerson({
+  getUUID: getId,    // Inject the ID generation function
+  getAge    // Inject the age calculation function
+});
+
+/**
+ * PERSON DATA: The data that will be passed to the created makePerson function
+ *
+ * This object contains the raw person data that makePerson expects.
+ * Notice how this is different from what buildMakePerson accepts - this is
+ * the data for the person, not the dependencies for the factory.
+ */
+const personObject = {
+  name: "John",
+  birthdate: new Date("1990-01-01"),
+};
+
+/**
+ * FACTORY PATTERN IN ACTION: Using the created function
+ *
+ * makePerson is the specialized function returned by buildMakePerson.
+ * It has the dependencies (getId, getAge) already "baked in" and is ready
+ * to create person objects from raw person data.
+ */
+console.log(makePerson(personObject));
--- 02-node-bases/src/js-bases/05-factory-functions.js
+++ 02-node-bases/src/js-bases/05-factory-functions.js
@@ -1,17 +1,28 @@
-import { getId, getAge } from '../plugins/index.plugin.js';
+// import { getId, getAge } from '../plugins/index.plugin.js';
 
-const buildPerson = ({ name, birthdate }) => {
-  return {
-    id: getId(),
-    birthdate: birthdate,
-    name,
-    age: getAge(birthdate),
-  };
+/**
+ * Factory Function with Dependency Injection Pattern
+ *
+ * This is a higher-order function that creates a specialized person-maker function.
+ * It follows the Factory Pattern by encapsulating object creation logic and the
+ * Dependency Injection pattern by accepting dependencies as parameters.
+ */
+const buildMakePerson = ({getUUID, getAge}) => {
+  // Return a new function that has the dependencies "baked in"
+  // This is called "partial application" - we're creating a specialized version
+  // of a more general function by pre-filling some of its parameters
+  return ({ name, birthdate }) => {
+    // Return a person object using the injected dependencies
+    // This is the actual person creation logic that gets executed
+    // when the returned function is called
+    return {
+      id: getUUID(),           // Use injected ID generation function
+      birthdate: birthdate,    // Pass through the birthdate
+      name,                    // Shorthand for name: name
+      age: getAge(birthdate),  // Use injected age calculation function
+    };
+  }
 };
 
-const personObject = {
-  name: "John",
-  birthdate: new Date("1990-01-01"),
-};
-
-export { buildPerson, personObject };
+// Export the factory function for use in other modules
+export { buildMakePerson };
